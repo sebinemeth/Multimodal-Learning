a343a7d3d6740123c20f0b9aa2992de06ca8d142
Sebi Nemeth <sebezhetetlen98@gmail.com>
2022-05-05 21:05:42 +0200

    update nv-prepare.sh

--- nv-prepare.sh
+++ nv-prepare.sh
@@ -6,13 +6,14 @@ for CLS in "$DATA_PATH""class_"*
 do
   for SEQ in "$CLS""/subject"*
   do
+    echo -n "$SEQ"
     for MOD in {color,depth}
     do
-      echo $MOD
+      echo -n "   $MOD"
       rm -r "$SEQ"/sk_"$MOD"_all
       mkdir -p "$SEQ"/sk_"$MOD"_all
-      ffmpeg -i "$SEQ"/sk_"$MOD".avi "$SEQ"/sk_"$MOD"_all/%05d.jpg
-      echo "$SEQ"
+      ffmpeg -hide_banner -loglevel error -i "$SEQ"/sk_"$MOD".avi "$SEQ"/sk_"$MOD"_all/%05d.jpg
     done
+    echo "   done"
   done
 done
