1cbcc840c8b72adcc81cc6df99d96f6693b15d3e
levopeti <leviskocka@gmail.com>
2022-10-27 15:39:51 +0200

    cm test 6

--- utils/confusion_matrix.py
+++ utils/confusion_matrix.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 def plot_confusion_matrix(_y_test, predictions, epoch, config_dict, post_fix=""):
     cm = confusion_matrix(_y_test, predictions, normalize="pred")
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=np.unique(_y_test))
-    disp.plot()
+    fig, ax = plt.subplots(figsize=(10, 10))
+    disp.plot(ax=ax)
     plt.savefig(os.path.join(config_dict["log_dir_path"], "cm_{}_{}.png".format(post_fix, epoch)), dpi=300)
 
 
