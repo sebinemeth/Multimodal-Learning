5e8507af25e65dfd2b28dae603ab856736c03176
levopeti <leviskocka@gmail.com>
2022-10-27 15:46:56 +0200

    cm test 9

--- utils/confusion_matrix.py
+++ utils/confusion_matrix.py
@@ -3,14 +3,20 @@ import numpy as np
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
+from utils.log_maker import write_log
+
 
 def plot_confusion_matrix(_y_test, predictions, epoch, config_dict, post_fix=""):
-    cm = confusion_matrix(_y_test, predictions, normalize="pred")
-    print(np.unique(_y_test))
-    print(np.unique(predictions))
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=np.unique(_y_test))
-    fig, ax = plt.subplots(figsize=(10, 10))
-    disp.plot(ax=ax)
-    plt.savefig(os.path.join(config_dict["log_dir_path"], "cm_{}_{}.png".format(post_fix, epoch)), dpi=300)
+    try:
+        cm = confusion_matrix(_y_test, predictions, normalize="pred")
+        print(np.unique(_y_test))
+        print(np.unique(predictions))
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=np.unique(_y_test))
+        fig, ax = plt.subplots(figsize=(10, 10))
+        disp.plot(ax=ax)
+        plt.savefig(os.path.join(config_dict["log_dir_path"], "cm_{}_{}.png".format(post_fix, epoch)), dpi=300)
+    except ValueError as e:
+        write_log("training", "error during plot confusion matrix: {}".format(e), title="plot cm")
+        return
 
 
