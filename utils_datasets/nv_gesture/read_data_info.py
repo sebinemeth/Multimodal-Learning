f33aee9e8a074a8398bf565f0368741b0cfe62fd
levopeti <leviskocka@gmail.com>
2022-11-14 16:52:44 +0100

    map classes 2

--- utils_datasets/nv_gesture/read_data_info.py
+++ utils_datasets/nv_gesture/read_data_info.py
@@ -71,16 +71,15 @@ def get_data_info_list(subset_type: SubsetType, config_dict: dict):
         max_frame_idx = annotations[i]['max_frame_idx']
         begin_t = int(annotations[i]['start_frame'])
         end_t = int(annotations[i]['end_frame'])
-
-        if len(config_dict["used_classes"]) > 0:
-            # map labels from 0 to num of classes - 1
-            label = config_dict["used_classes"].index(int(annotations[i]['label']))
-        else:
-            label = int(annotations[i]['label'])
+        label = int(annotations[i]['label'])
 
         if len(config_dict["used_classes"]) > 0 and label not in config_dict["used_classes"]:
             # if config_dict["used_classes"] is empty, each class will be used
             continue
+
+        if len(config_dict["used_classes"]) > 0:
+            # map labels from 0 to num of classes - 1
+            label = config_dict["used_classes"].index(label)
 
         frame_jump = config_dict["frame_jump"]
         sample_duration = config_dict["sample_duration"]
