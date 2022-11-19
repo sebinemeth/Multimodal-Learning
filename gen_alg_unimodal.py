3c815c984be3b70fdde34f8fd835ece6be99a56f
levopeti <leviskocka@gmail.com>
2022-11-19 23:43:14 +0100

    requ fix

--- gen_alg_unimodal.py
+++ gen_alg_unimodal.py
@@ -29,7 +29,7 @@ def fitness(x):
         yaml.dump(config_dict, outfile, default_flow_style=False)
 
     history = main()
-    return history.get_last((SubsetType.VAL, ModalityType.RGB, MetricType.LOSS))
+    return history.get_epoch_last_item((SubsetType.VAL, ModalityType.RGB, MetricType.LOSS))
 
 
 var_bounds = np.array([[3, 5],      # weight_decay (exp)
