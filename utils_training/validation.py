1e9d5ad0bd702eae9156817701a28ff33a75b904
levopeti <leviskocka@gmail.com>
2022-11-27 00:02:10 +0100

    vol 22

--- utils_training/validation.py
+++ utils_training/validation.py
@@ -54,13 +54,13 @@ def validation_step(model_dict: Dict[ModalityType, Module],
             tq.update(1)
             tq.set_postfix(**convert_to_tqdm_dict(tqdm_dict))
 
-        # for key in tqdm_dict.keys():
-        #     if key[2] == MetricType.LOSS:
-        #         history.add_epoch_items({key: mean(loss_dict[key[1]])})
-        #     else:
-        #         history.add_epoch_items({key: tqdm_dict[key]})
+        for key in tqdm_dict.keys():
+            if key[2] == MetricType.LOSS:
+                history.add_epoch_items({key: mean(loss_dict[key[1]])})
+            else:
+                history.add_epoch_items({key: tqdm_dict[key]})
 
-        plot_confusion_matrix(y_test, predictions_dict, epoch, SubsetType.VAL, config_dict)
+        #plot_confusion_matrix(y_test, predictions_dict, epoch, SubsetType.VAL, config_dict)
 
 
 # def validation_step_old(model_rgb: nn.Module, model_depth: nn.Module, criterion, valid_loader, epoch, config_dict):
