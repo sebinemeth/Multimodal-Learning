556330a809754a4d7ce078efe3c8e96477ca61e3
levopeti <leviskocka@gmail.com>
2022-11-27 22:46:31 +0100

    before depth

--- train_main.py
+++ train_main.py
@@ -1,8 +1,9 @@
 import os
+import warnings
+import traceback
 import torch
 from torch.nn import Module
 from torchsummary import summary
-import traceback
 from typing import Dict
 
 from utils.log_maker import start_log_maker, write_log
@@ -15,74 +16,8 @@ from utils.history import History
 from utils.callbacks import Tensorboard, SaveModel, EarlyStopping, CallbackRunner, EarlyStopException
 from utils_datasets.nv_gesture.nv_utils import SubsetType, ModalityType, MetricType
 
-import warnings
 # to supress pytorch nn.MaxPool3d warning
 warnings.filterwarnings("ignore", category=UserWarning)
-
-
-def write_end_log(discord: DiscordBot, text: str, title: str):
-    discord.send_message(fields=[{"name": title, "value": text, "inline": True}])
-    write_log("training", text, title=title, print_out=True, color="red")
-
-
-def convert_modalities(config_dict: dict):
-    modalities = list()
-    for modality in config_dict["modalities"]:
-        if modality == "RGB":
-            modalities.append(ModalityType.RGB)
-        elif modality == "DEPTH":
-            modalities.append(ModalityType.DEPTH)
-        else:
-            raise ValueError("unknown modality: {}".format(modality))
-
-    config_dict["modalities"] = modalities
-
-
-def get_history(config_dict: dict, discord: DiscordBot) -> History:
-    epoch_keys = list()
-    batch_keys = list()
-    for modality in config_dict["modalities"]:
-        epoch_keys.extend([(SubsetType.TRAIN, modality, MetricType.LOSS),
-                           (SubsetType.TRAIN, modality, MetricType.ACC),
-                           (SubsetType.VAL, modality, MetricType.LOSS),
-                           (SubsetType.VAL, modality, MetricType.ACC)])
-        batch_keys.extend([(SubsetType.TRAIN, modality, MetricType.LOSS),
-                           (SubsetType.TRAIN, modality, MetricType.ACC)])
-
-        if len(config_dict["modalities"]) > 1:
-            epoch_keys.append((SubsetType.TRAIN, modality, MetricType.REG_LOSS))
-            batch_keys.append((SubsetType.TRAIN, modality, MetricType.REG_LOSS))
-
-    history = History(config_dict=config_dict,
-                      epoch_keys=epoch_keys,
-                      batch_keys=batch_keys,
-                      discord=discord)
-    return history
-
-
-def get_callback_runner(model_dict: Dict[ModalityType, Module], history: History, config_dict: dict) -> CallbackRunner:
-    callback_list = [EarlyStopping(
-        history=history,
-        config_dict=config_dict,
-        key=(SubsetType.VAL,
-             config_dict["modalities"][0] if len(config_dict["modalities"]) == 1 else ModalityType.RGB_DEPTH,
-             MetricType.LOSS),
-        patience=4,
-        delta=0.02)]
-
-    for modality in config_dict["modalities"]:
-        callback_list.extend([SaveModel(history=history,
-                                        model=model_dict[modality],
-                                        modality=modality,
-                                        config_dict=config_dict,
-                                        only_best_key=(SubsetType.VAL, modality, MetricType.LOSS)),
-                              Tensorboard(history=history,
-                                          config_dict=config_dict,
-                                          batch_end_keys=[(SubsetType.TRAIN, modality, MetricType.LOSS),
-                                                          (SubsetType.TRAIN, modality, MetricType.ACC)],
-                                          epoch_end_keys=[(SubsetType.VAL, modality, MetricType.LOSS),
-                                                          (SubsetType.VAL, modality, MetricType.ACC)])])
-    return CallbackRunner(callbacks=callback_list)
 
 
 def main() -> History:
@@ -139,6 +74,71 @@ def main() -> History:
         callback_runner.on_training_end()
 
     return history
+
+
+def write_end_log(discord: DiscordBot, text: str, title: str):
+    discord.send_message(fields=[{"name": title, "value": text, "inline": True}])
+    write_log("training", text, title=title, print_out=True, color="red")
+
+
+def convert_modalities(config_dict: dict):
+    modalities = list()
+    for modality in config_dict["modalities"]:
+        if modality == "RGB":
+            modalities.append(ModalityType.RGB)
+        elif modality == "DEPTH":
+            modalities.append(ModalityType.DEPTH)
+        else:
+            raise ValueError("unknown modality: {}".format(modality))
+
+    config_dict["modalities"] = modalities
+
+
+def get_history(config_dict: dict, discord: DiscordBot) -> History:
+    epoch_keys = list()
+    batch_keys = list()
+    for modality in config_dict["modalities"]:
+        epoch_keys.extend([(SubsetType.TRAIN, modality, MetricType.LOSS),
+                           (SubsetType.TRAIN, modality, MetricType.ACC),
+                           (SubsetType.VAL, modality, MetricType.LOSS),
+                           (SubsetType.VAL, modality, MetricType.ACC)])
+        batch_keys.extend([(SubsetType.TRAIN, modality, MetricType.LOSS),
+                           (SubsetType.TRAIN, modality, MetricType.ACC)])
+
+        if len(config_dict["modalities"]) > 1:
+            epoch_keys.append((SubsetType.TRAIN, modality, MetricType.REG_LOSS))
+            batch_keys.append((SubsetType.TRAIN, modality, MetricType.REG_LOSS))
+
+    history = History(config_dict=config_dict,
+                      epoch_keys=epoch_keys,
+                      batch_keys=batch_keys,
+                      discord=discord)
+    return history
+
+
+def get_callback_runner(model_dict: Dict[ModalityType, Module], history: History, config_dict: dict) -> CallbackRunner:
+    callback_list = [EarlyStopping(
+        history=history,
+        config_dict=config_dict,
+        key=(SubsetType.VAL,
+             config_dict["modalities"][0] if len(config_dict["modalities"]) == 1 else ModalityType.RGB_DEPTH,
+             MetricType.LOSS),
+        patience=config_dict["patience"],
+        delta=config_dict["delta"])]
+
+    for modality in config_dict["modalities"]:
+        callback_list.extend([SaveModel(history=history,
+                                        model=model_dict[modality],
+                                        modality=modality,
+                                        config_dict=config_dict,
+                                        only_best_key=(SubsetType.VAL, modality, MetricType.LOSS)),
+                              Tensorboard(history=history,
+                                          config_dict=config_dict,
+                                          batch_end_keys=[(SubsetType.TRAIN, modality, MetricType.LOSS),
+                                                          (SubsetType.TRAIN, modality, MetricType.ACC)],
+                                          epoch_end_keys=[(SubsetType.VAL, modality, MetricType.LOSS),
+                                                          (SubsetType.VAL, modality, MetricType.ACC)])])
+    return CallbackRunner(callbacks=callback_list)
 
 
 if __name__ == "__main__":
